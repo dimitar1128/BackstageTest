04a353159c054531ee6aa762659d36333d4d6b4c
dimitar1128 <dimi.rangev09@gmail.com>
2021-02-10 00:40:20 +0200

    readme requirement test

---
+++ README.md
@@ -0,0 +1,44 @@
+## Backstage Test ##
+###How to run
+- Download and install python (>3.7). <br>
+https://www.python.org/downloads/ <br>
+Make sure to install `pip` (python package management tool) during installing python.
+- Clone the project from its repository.
+- In the project base folder run below command to install dependencies. <br>
+`pip install -r requirements.txt`
+- Migrate database
+`python manage.py migrate`
+- Run the server with the below command.
+`python manage.py runserver` <br>
+This should run the server on http://localhost:8000
+- Test the endpoint with the below command.
+curl --request GET 'http://localhost:8000/difference?number=10' <br>
+
+Expected result.
+```text
+{
+    "datetime": "2021-02-09 23:17:05",
+    "value": 2640,
+    "number": 10,
+    "occurrences": 1
+}
+```
+
+- Access http://localhost:8000 with browser. You will see the UI to query and see the result.
+Type number in the input and click "query" button.
+The result will be shown in the table.
+
+
+### How it works ###
+
+- This project uses sqlite3 for its database.
+- When the request is received, it first checks the database if the "number" is already stored.
+If then it adds 1 to the "occurrences" value of the "number" and return result.
+If "number" is not stored in the database, which means the "number" has not requested yet, calculate the different value (between the sum of the squares of the first n natural numbers and the square of the sum of the same first n natural numbers) and store it in the database.
+
+### Unit Test ###
+`python manage.py test apps.service.tests.ServiceTestCase.test_service` <br>
+
+
+
+
--- apps/service/tests.py
+++ apps/service/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+import pandas as pd
+import random
 
-# Create your tests here.
+from django.test import TestCase
+from django.test.client import Client
+
+class ServiceTestCase(TestCase):
+
+    def test_service(self):
+        client = Client()
+        url = '/difference'
+        number = random.randint(1, 100)
+        res = client.get(url, data={
+            'number': number
+        })
+
+        # check status code
+        assert res.status_code == 200, f'Endpoint returns unexpected status code. {res.status_code}'
+        res = res.json()
+
+        # check columns
+        df = pd.DataFrame([res])
+        expected_columns =[
+            'datetime',
+            'value',
+            'number',
+            'occurrences',
+        ]
+        assert set(expected_columns) == set(df.columns), 'Endpoint returns unexpected columns'
+
+        # check values
+        assert res['number'] ==  number, f'Endpoint returns invalid number. Expected: {number}, Actual: {res["number"]}'
+
+        sum1 = 0
+        sum2 = 0
+        for i in range(1, number+1):
+            sum1 += (i ** 2)
+        for i in range(1, number+1):
+            sum2 += i
+        sum2 = sum2 ** 2
+
+        exp_value = sum2 - sum1
+        assert res['value'] == exp_value, f'Endpoint returns invalid value. Expected: {exp_value}, Actual: {res["value"]}'
+
+        assert res['occurrences'] == 1, f'Endpoint returns invalid occurrences. Expected: 1, Actual: {res["occurrences"]}'
+
+        # try again, and check 'occurrences' if it is increased
+        res = client.get(url, data={
+            'number': number
+        })
+
+        # check status code
+        assert res.status_code == 200, f'Endpoint returns unexpected status code. {res.status_code}'
+        res = res.json()
+        assert res['occurrences'] == 2, f'Endpoint returns invalid occurrences. Expected: 2, Actual: {res["occurrences"]}'
+
+
+
+
+
+
